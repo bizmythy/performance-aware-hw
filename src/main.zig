4c6ca76a590e7ad8c5a33ec107631656133cdded
bizmythy <andrew.p.council@gmail.com>
2025-10-05 16:44:56 -0400

    basic read byte by byte
    new i/o, very nice!

--- src/main.zig
+++ src/main.zig
@@ -2,26 +2,35 @@ const std = @import("std");
 const performance_aware_hw = @import("performance_aware_hw");
 
 pub fn main() !void {
-    // Prints to stderr, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-    try performance_aware_hw.bufferedPrint();
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+    const dir_options: std.fs.Dir.OpenOptions = .{
+        .access_sub_paths = true,
+        .iterate = false,
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    const perfaware_dir = try std.fs.openDirAbsolute("/home/drew/personal/computer_enhance/perfaware", dir_options);
+    const part_1 = try perfaware_dir.openDir("part1", dir_options);
+
+    try readInstructions(part_1, "listing_0037_single_register_mov");
+    try readInstructions(part_1, "listing_0038_many_register_mov");
+}
+
+fn readInstructions(dir: std.fs.Dir, exe: []const u8) !void {
+    std.debug.print("Reading instructions from {s}\n", .{exe});
+    const exe_file = try dir.openFile(exe, .{ .mode = .read_only });
+
+    var buf: [1024 * 1024]u8 = undefined;
+    var reader = exe_file.reader(&buf);
+
+    while (true) {
+        // pop next byte from reader
+        const byte = reader.interface.takeByte() catch |err| {
+            const etype = std.Io.Reader.Error;
+            switch (err) {
+                etype.EndOfStream => break,
+                etype.ReadFailed => return err,
+            }
+        };
+        std.debug.print("byte read: {b}\n", .{byte});
+
+        // interpret byte
+    }
 }
