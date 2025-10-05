a110f9141570f379d341238840a53508961f4654
bizmythy <andrew.p.council@gmail.com>
2025-10-05 16:50:33 -0400

    cleanup

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,22 @@
 const std = @import("std");
+const fs = std.fs;
+const print = std.debug.print;
 const performance_aware_hw = @import("performance_aware_hw");
 
 pub fn main() !void {
-    const dir_options: std.fs.Dir.OpenOptions = .{
+    const dir_options: fs.Dir.OpenOptions = .{
         .access_sub_paths = true,
         .iterate = false,
     };
-    const perfaware_dir = try std.fs.openDirAbsolute("/home/drew/personal/computer_enhance/perfaware", dir_options);
+    const perfaware_dir = try fs.openDirAbsolute("/home/drew/personal/computer_enhance/perfaware", dir_options);
     const part_1 = try perfaware_dir.openDir("part1", dir_options);
 
     try readInstructions(part_1, "listing_0037_single_register_mov");
     try readInstructions(part_1, "listing_0038_many_register_mov");
 }
 
-fn readInstructions(dir: std.fs.Dir, exe: []const u8) !void {
-    std.debug.print("Reading instructions from {s}\n", .{exe});
+fn readInstructions(dir: fs.Dir, exe: []const u8) !void {
+    print("Reading instructions from {s}\n", .{exe});
     const exe_file = try dir.openFile(exe, .{ .mode = .read_only });
 
     var buf: [1024 * 1024]u8 = undefined;
@@ -29,7 +31,7 @@ fn readInstructions(dir: std.fs.Dir, exe: []const u8) !void {
                 etype.ReadFailed => return err,
             }
         };
-        std.debug.print("byte read: {b}\n", .{byte});
+        print("byte read: {b}\n", .{byte});
 
         // interpret byte
     }
