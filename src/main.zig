d81d38dbc5d9335895ae281af50e66bbf5ed7b33
bizmythy <andrew.p.council@gmail.com>
2025-10-05 23:38:32 -0400

    fully working asm parser

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,12 @@
 const std = @import("std");
 const fs = std.fs;
-const print = std.debug.print;
+
+fn print(comptime fmt: []const u8, args: anytype) void {
+    if (false) {
+        std.debug.print(fmt, args);
+    }
+}
+
 const performance_aware_hw = @import("performance_aware_hw");
 
 pub fn main() !void {
@@ -11,7 +17,7 @@ pub fn main() !void {
     const perfaware_dir = try fs.openDirAbsolute("/home/drew/personal/computer_enhance/perfaware", dir_options);
     const part_1 = try perfaware_dir.openDir("part1", dir_options);
 
-    try readInstructions(part_1, "listing_0037_single_register_mov");
+    // try readInstructions(part_1, "listing_0037_single_register_mov");
     try readInstructions(part_1, "listing_0038_many_register_mov");
 }
 
@@ -73,12 +79,20 @@ fn readInstructions(dir: fs.Dir, exe: []const u8) !void {
     print("Reading instructions from {s}\n", .{exe});
     const exe_file = try dir.openFile(exe, .{ .mode = .read_only });
 
-    var buf: [1024 * 1024]u8 = undefined;
-    var reader = exe_file.reader(&buf);
+    var reader_buf: [1024 * 1024]u8 = undefined;
+    var reader = exe_file.reader(&reader_buf);
+    const instructions = &reader.interface;
+
+    var writer_buf: [1024 * 1024]u8 = undefined;
+    var writer = fs.File.stdout().writer(&writer_buf);
+    const stdout = &writer.interface;
+
+    // indicate that this is for 16 bit to NASM
+    try stdout.writeAll("bits 16\n");
 
     while (true) {
         // pop next byte from reader
-        const byte = reader.interface.takeByte() catch |err| {
+        const byte = instructions.takeByte() catch |err| {
             const etype = std.Io.Reader.Error;
             switch (err) {
                 etype.EndOfStream => break,
@@ -90,14 +104,16 @@ fn readInstructions(dir: fs.Dir, exe: []const u8) !void {
         if (Mov.getIfMatch(byte)) |mov| {
             print("MOV instruction found: {b} {b}\n", .{ mov.d, mov.w });
 
-            const mov2_byte = try reader.interface.takeByte();
+            const mov2_byte = try instructions.takeByte();
             const mov2: Mov2 = @bitCast(mov2_byte);
             print("MOV args found: {b} {b} {b}\n", .{ mov2.r_w, mov2.reg, mov2.mod });
 
             const register_r_w = Register.get(mov2.r_w, mov.w);
             const register_reg = Register.get(mov2.reg, mov.w);
             const registers = if (mov.d == 1) .{ register_reg, register_r_w } else .{ register_r_w, register_reg };
-            print("mov {t}, {t}\n", .{ registers[0], registers[1] });
+            try stdout.print("mov {t}, {t}\n", .{ registers[0], registers[1] });
         }
     }
+
+    try stdout.flush();
 }
